5ac6eec107f25f6ec113eceb24f1cefa68ee5cde
Neftaly <neftalyrv23@gmail.com>
2023-04-03 20:42:20 -0400

    Made the choice array and the function that make the random computer selection

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # rock-paper-scissor-game
+This will be a basic game that will be played in the console.
---
+++ javascript.js
@@ -0,0 +1,8 @@
+let choices = ["rock","paper","scissor"]
+
+function computerChoice() {
+    let choice = choices[Math.floor(Math.random() * choices.length)]
+    return choice
+}
+
+
