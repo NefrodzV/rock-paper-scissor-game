f02085ef45dceae61eca77e733fd9d232046275b
Neftaly <neftalyrv23@gmail.com>
2023-04-03 20:44:14 -0400

    Did the function for when the player makes a choice and the computer also and gives the appropiate conclusion between the two and gives the win or lose result as a string.

--- javascript.js
+++ javascript.js
@@ -5,4 +5,57 @@ function computerChoice() {
     return choice
 }
 
+function play (playerChoice, computerChoice) {
+    let choice = playerChoice.toLowerCase()
+    switch(playerChoice) {
+        case "rock":
+            if(computerChoice == "scissor") {
+               return "You win! Rock beats scissor."
+            }
+
+            if(computerChoice == "paper") {
+                return "You lose! Paper beats rock."
+            }
+
+            if (computerChoice == "rock") {
+                return "Draw!"
+            }
+        break;
+
+        case "paper":
+            if(computerChoice == "scissor") {
+                return "You lose! Scissor beats paper."
+            }
+ 
+            if(computerChoice == "paper") {
+                return "Draw!"
+            }
+ 
+            if (computerChoice == "rock") {
+                return "You win! Paper beats rock."
+            }
+
+        break;
+
+        case "scissor":
+            if(computerChoice == "scissor") {
+                return "Draw!"
+            }
+ 
+            if(computerChoice == "paper") {
+                return "You win! Scissor beats paper."
+            }
+ 
+            if (computerChoice == "rock") {
+                return "You lose! Rock beats scissor."
+            }
+        break;
+
+        default:
+            return "Please make a appropiate selection! Type rock, paper or scissor"
+        
+    }
+}
+
+
 
